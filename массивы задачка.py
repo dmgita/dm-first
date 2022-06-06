64c28989fce92366c6fc0db05b7e4b3374334ea1
Dina <dina_keep@bk.ru>
2022-06-06 09:16:59 +0300

    Create массивы задачка.py
    версия 0

---
+++ массивы задачка.py
@@ -0,0 +1,29 @@
+mass = [4, 1, 8, 9, 5, 4, 3]
+c = 0
+nc = 0
+sum = 0
+pr = mass[1-1] * mass[3-1] * mass[6-1]
+for i in mass:
+    sum += i
+    if i%2 ==0:
+        c = c+1
+    else:
+        nc = nc+1
+if c > nc:
+    print ('сумма', sum)
+else:
+    print ('произведение', pr)
+
+
+
+
+# for n in range (1,10):
+#     for i in range(1,5):
+#         print(i, "x", n, "=", n*i, end = '\t')
+#     print ('')
+# print ('')    
+# for n in range (1,10):   
+#     for i in range(5,9):
+#         print(i, "x", n, "=", n*i, end = '\t')
+#     print ('')
+
